569b99d92b0c4dd42af36ddacd9edbf481d185cf
HarshaVijayakumar <pinkyy.25@gmail.com>
2019-05-24 16:46:26 +0800

    understanding Html

---
+++ simple_html.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+
+SIMPLE_HTML = '''<html>
+<head></head>
+<body>
+<h1>This is a title</h1>
+<p class="subtitle">Lorem ipsum dolor sit amet. Consectetur edipiscim elit.</p>
+<p>Here's another p without a class</p>
+<ul>
+    <li>Rolf</li>
+    <li>Charlie</li>
+    <li>Jen</li>
+    <li>Jose</li>
+</ul>
+</body>
+</html>'''
+
+simple_soup = BeautifulSoup(SIMPLE_HTML, 'html.parser')
+
+
+def find_title():
+
+    h1_tag = simple_soup.find('h1')
+    print(simple_soup.find('h1'))
+    print(simple_soup.find('h1').string)
+    print(h1_tag.string)
+
+
+def find_list_items():
+    list_items = simple_soup.find_all('li')
+    print(list_items)
+
+
+find_title()
+find_list_items()
